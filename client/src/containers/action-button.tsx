4d82b166b1c87fcbac18d5b2c6ab1b0a7264dc66
naren <blazeraze.naren@gmail.com>
2020-07-18 14:11:38 +0545

    local resolver for removing/adding item to cart

--- client/src/containers/action-button.tsx
+++ client/src/containers/action-button.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import {useMutation} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { GET_LAUNCH_DETAILS } from '../pages/launch';
+import Button from '../components/button';
+import * as LaunchDetailTypes from '../pages/__generated__/LaunchDetails';
+
+export const TOGGLE_CART = gql`
+  mutation addOrRemoveFromCart($launchId: ID!){
+    addOrRemoveFromCart(launchId: $launchId) @client
+  }
+`
 
 const ActionButton: React.FC<any> = () => {
   return <div/>;
